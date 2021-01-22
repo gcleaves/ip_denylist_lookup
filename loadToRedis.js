2342e2816a490b66e4df68cbb9c4d89356565905
gcleaves <gcleaves@gmail.com>
2021-01-22 09:22:36 +0100

    improvement: loadToRedis as worker thread

--- loadToRedis.js
+++ loadToRedis.js
@@ -1,146 +1,170 @@
 'use strict';
-const csv = require('csv-parser');
-const fs = require('fs');
-const Redis = require("ioredis");
-const { format} = require('date-fns');
-const nowFormat = () => format(new Date(),'yyyy-MM-dd HH:mm:ss');
+const { Worker, isMainThread,  workerData, parentPort } = require('worker_threads');
+const pluginName = 'load_to_redis';
 
-function forceGC() {
-   if (global.gc) {
-      global.gc();
-   } else {
-      console.warn('No GC hook! Start your program as `node --expose-gc file.js`.');
-   }
-}
+if(isMainThread) {
+    exports.load = (file, redisPrefix, gc) => {
+        return new Promise((resolve, reject) => {
+            const worker = new Worker(__filename, {
+                workerData: {file, redisPrefix, gc},
+                execArgv: ['--unhandled-rejections=strict']
+            });
+            worker.on("message", data=>resolve(data));
+            worker.on("error", error=>{
+                reject(error)
+            });
+            worker.on("exit", code=>console.log(`${pluginName} exited with code: ${code}`));
+        });
+    };
+} else {
+    const csv = require('csv-parser');
+    const fs = require('fs');
+    const Redis = require("ioredis");
+    const { format} = require('date-fns');
+    const nowFormat = () => format(new Date(),'yyyy-MM-dd HH:mm:ss');
 
-function ip2int(ip) {
-    return ip.split('.').reduce(function(ipInt, octet) { return (ipInt<<8) + parseInt(octet, 10)}, 0) >>> 0;
-}
+    function forceGC() {
+        if (global.gc) {
+            global.gc();
+        } else {
+            console.warn('No GC hook! Start your program as `node --expose-gc file.js`.');
+        }
+    }
 
-exports.load = (file, redisPrefix, gc) => {
-    //console.log('gc=%s',gc);
-	let k = 0;
-    const scratch = [];
-    return new Promise((resolve, reject) => {
-        const redis = new Redis({
-            host:process.env.REDIS_HOST,
-            port: process.env.REDIS_PORT, // Redis port
-            family: process.env.REDIS_IP_FAMILY, // 4 (IPv4) or 6 (IPv6)
-            password: process.env.REDIS_PASS,
-            db: process.env.REDIS_DB,
-        }); // uses defaults unless given configuration object
-        const tempKey = 'arfa45e13grh785gEV4wfw$WF7h';
-        redis.del(tempKey);
-        fs.createReadStream(file, {endX: 10000})
-            .pipe(csv({
-                separator: '|',
-                quote: '~',
-                mapValues: ({header, index, value}) => {
-                    if (header === 'list') {
-                        //if(!value || value=='') return 'unknown';
-                        //return value.trim();
-                        return value;
-                    } else {
-                        return parseInt(value);
+    function ip2int(ip) {
+        return ip.split('.').reduce(function(ipInt, octet) { return (ipInt<<8) + parseInt(octet, 10)}, 0) >>> 0;
+    }
+
+    const main = (file, redisPrefix, gc) => {
+        //console.log('gc=%s',gc);
+        let k = 0;
+        const scratch = [];
+        return new Promise((resolve, reject) => {
+            const redis = new Redis({
+                host:process.env.REDIS_HOST,
+                port: process.env.REDIS_PORT, // Redis port
+                family: process.env.REDIS_IP_FAMILY, // 4 (IPv4) or 6 (IPv6)
+                password: process.env.REDIS_PASS,
+                db: process.env.REDIS_DB,
+            }); // uses defaults unless given configuration object
+            const tempKey = 'arfa45e13grh785gEV4wfw$WF7h';
+            redis.del(tempKey);
+            fs.createReadStream(file, {endX: 10000})
+                .pipe(csv({
+                    separator: '|',
+                    quote: '~',
+                    mapValues: ({header, index, value}) => {
+                        if (header === 'list') {
+                            //if(!value || value=='') return 'unknown';
+                            //return value.trim();
+                            return value;
+                        } else {
+                            return parseInt(value);
+                        }
                     }
-                }
-            }))
-            .on('data', (r) => {
-                //console.log(r);
-                k++;
-                if (!(k % 10000)) {
-                    //process.stdout.write(`reading CSV line: ${k}\r`);
-                    console.log(nowFormat() + `| reading CSV line: ${k}`);
-                }
-                scratch.push({n: r.start_int, a: r.list, e: false});
-                scratch.push({n: r.end_int, a: r.list, e: true});
-            })
-            .on('end', async () => {
-                redis.lpush(redisPrefix + 'ipListSize', JSON.stringify({
-                    "date": format(new Date(), 'yyyy-MM-dd HH:mm:ss'),
-                    "size": k,
-                    "lists": await redis.smembers(redisPrefix + 'lists')
-                }));
-                console.log(nowFormat() + '| CSV file successfully processed %s lines', k);
-                console.log(nowFormat() + "| " + scratch.length);
-                console.log(nowFormat() + '| Sorting...');
-                scratch.sort((a, b) => {
-                    if (a.n < b.n) return -1;
-                    if (a.n > b.n) return 1;
-                    if (a.e < b.e) {
-                        return -1
-                    }
-                    if (a.e > b.e) {
-                        return 1
-                    }
-                    return 0;
-                });
-                console.log(nowFormat() + '| Sorting finished');
-                //console.log(scratch);
-
-                let s = [];
-                let n;
-                let m;
-				let plRes;
-
-                let pipeline = redis.pipeline();
-                for (let k = 0; k < scratch.length - 1; k++) {
+                }))
+                .on('data', (r) => {
+                    //console.log(r);
+                    k++;
                     if (!(k % 10000)) {
-                        //process.stdout.write(`flattening ${k} of ${scratch.length}\r`);
-                        console.log(nowFormat() + `| flattening ${k} of ${scratch.length}`);
+                        //process.stdout.write(`reading CSV line: ${k}\r`);
+                        console.log(nowFormat() + `| reading CSV line: ${k}`);
                     }
-                    if (!(k % 100000)) {
-                        await pipeline.exec();
-						//plRes = null;
-						if(gc) forceGC();
-                        pipeline = redis.pipeline(); //.client('reply', 'off');
-                    }
-                    let cur = scratch[k];
-                    let nex = scratch[k + 1];
-                    if (cur.e === false) {
-                        s.push(cur.a);
-                    } else {
-                        let index = s.indexOf(cur.a);
-                        if (index > -1) s.splice(index, 1);
-                    }
-                    if (cur.e === false) {
-                        n = cur.n;
-                    } else {
-                        n = cur.n + 1;
-                    }
-                    if (nex.e === false) {
-                        m = nex.n - 1;
-                    } else {
-                        m = nex.n;
-                    }
+                    scratch.push({n: r.start_int, a: r.list, e: false});
+                    scratch.push({n: r.end_int, a: r.list, e: true});
+                })
+                .on('end', async () => {
+                    redis.lpush(redisPrefix + 'ipListSize', JSON.stringify({
+                        "date": format(new Date(), 'yyyy-MM-dd HH:mm:ss'),
+                        "size": k,
+                        "lists": await redis.smembers(redisPrefix + 'lists')
+                    }));
+                    console.log(nowFormat() + '| CSV file successfully processed %s lines', k);
+                    console.log(nowFormat() + "| " + scratch.length);
+                    console.log(nowFormat() + '| Sorting...');
+                    scratch.sort((a, b) => {
+                        if (a.n < b.n) return -1;
+                        if (a.n > b.n) return 1;
+                        if (a.e < b.e) {
+                            return -1
+                        }
+                        if (a.e > b.e) {
+                            return 1
+                        }
+                        return 0;
+                    });
+                    console.log(nowFormat() + '| Sorting finished');
+                    //console.log(scratch);
 
-                    if (n <= m && s.length) {
-						s = [...new Set(s)];
-						//console.log(s);
+                    let s = [];
+                    let n;
+                    let m;
+                    let plRes;
 
-						let data = {}; // geo: [], list: [], asn: []
-						for(const i of s) {
-						    //console.log(i);
-						    const j = JSON.parse(i);
-						    const type = j.type;
-						    delete j.type;
-						    //console.log(j);
-                            if(!data[type]) data[type] = [];
-						    data[type].push(j);
+                    let pipeline = redis.pipeline();
+                    for (let k = 0; k < scratch.length - 1; k++) {
+                        if (!(k % 10000)) {
+                            //process.stdout.write(`flattening ${k} of ${scratch.length}\r`);
+                            console.log(nowFormat() + `| flattening ${k} of ${scratch.length}`);
+                        }
+                        if (!(k % 100000)) {
+                            await pipeline.exec();
+                            //plRes = null;
+                            if(gc) forceGC();
+                            pipeline = redis.pipeline(); //.client('reply', 'off');
+                        }
+                        let cur = scratch[k];
+                        let nex = scratch[k + 1];
+                        if (cur.e === false) {
+                            s.push(cur.a);
+                        } else {
+                            let index = s.indexOf(cur.a);
+                            if (index > -1) s.splice(index, 1);
+                        }
+                        if (cur.e === false) {
+                            n = cur.n;
+                        } else {
+                            n = cur.n + 1;
+                        }
+                        if (nex.e === false) {
+                            m = nex.n - 1;
+                        } else {
+                            m = nex.n;
                         }
 
-                        pipeline.zadd(tempKey, m, `${n}|${m}|${JSON.stringify(data)}`);
-                    }
-                }
-                await pipeline.exec();
-                await redis.rename(tempKey, redisPrefix + 'ranges');
-                await redis.del(tempKey);
+                        if (n <= m && s.length) {
+                            s = [...new Set(s)];
+                            //console.log(s);
 
-                redis.client('reply','on').then( a=> {
-                    redis.disconnect();
-                    resolve();
+                            let data = {}; // geo: [], list: [], asn: []
+                            for(const i of s) {
+                                //console.log(i);
+                                const j = JSON.parse(i);
+                                const type = j.type;
+                                delete j.type;
+                                //console.log(j);
+                                if(!data[type]) data[type] = [];
+                                data[type].push(j);
+                            }
+
+                            pipeline.zadd(tempKey, m, `${n}|${m}|${JSON.stringify(data)}`);
+                        }
+                    }
+                    await pipeline.exec();
+                    await redis.rename(tempKey, redisPrefix + 'ranges');
+                    await redis.del(tempKey);
+
+                    redis.client('reply','on').then( a=> {
+                        redis.disconnect();
+                        resolve();
+                    });
                 });
-            });
-    });
-};
+        });
+    };
+
+    main(workerData.file, workerData.redisPrefix, workerData.gc)
+        .then(data => parentPort.postMessage(`${pluginName} done.`))
+        .catch(error => {throw error});
+}
+
 
--- plugins/udger_stale/index.js
+++ plugins/udger_stale/index.js
@@ -1,87 +1,111 @@
 // https://udger.com/ - datacenter IP database
-
 'use strict';
-const axios = require('axios');
-const fs = require('fs');
-const sqlite3 = require('sqlite3').verbose();
-const ip = require('ip-utils');
-const dbFile = __dirname + '/' + 'udgerdb_v3.dat';
+const { Worker, isMainThread,  workerData, parentPort } = require('worker_threads');
 const pluginName = 'udger_stale';
 
-module.exports = async (outputFile) => {
-    const interval = setInterval(()=>console.log("still working on " + pluginName),5000);
-    try {
-
-        console.log('extracting datacenter IPs');
-        const tempFile = __dirname + '/' + 'rth595hg34G4gsdfgnu7865y';
-        const writer = fs.createWriteStream(tempFile);
-        let db;
-
-        await Promise.all([
-            new Promise((resolve, reject) =>  {
-                db = new sqlite3.Database(dbFile, sqlite3.OPEN_READONLY, (error) => {
-                    if(error) reject(pluginName + ' failed: ' + error.message)
-                    resolve();
-                });
-            }),
-            new Promise((resolve, reject) => {
-                db.each("SELECT iplong_from `from`, iplong_to `to`, name from udger_datacenter_range r " +
-                    "inner join udger_datacenter_list l on r.datacenter_id=l.id limit -1", function (err, row) {
-                    const meta = {
-                        type: "list",
-                        source: "udger",
-                        name: "datacenter",
-                        meta: {
-                            "datacenter_name": row.name
-                        }
-                    };
-                    const metadata = JSON.stringify(meta);
-
-                    //let dcName = row.name.replace(/"/g, '\"');
-                    //dcName = dcName.replace(/,/g, '');
-                    const csv = row.from + "|" + row.to + "|" + metadata + "\n"; // -${row.name}
-                    writer.write(csv);
-                }, resolve);
-            }),
-            new Promise((resolve, reject) => {
-                db.each("SELECT l.ip, c.ip_classification_code code, l.ip_country_code country, l.ip_city city from udger_ip_list l " +
-                    "inner join udger_ip_class c on l.class_id=c.id limit -1", function (err, row) {
-                    const meta = {
-                        type: "geo",
-                        country: row.country,
-                        city: row.city,
-                        source: "udger"
-                    };
-                    let metadata = JSON.stringify(meta);
-                    let csv = `${ip.toLong(row.ip)}|${ip.toLong(row.ip)}|` + metadata + "\n";
-                    writer.write(csv);
-
-                    delete meta.country;
-                    delete meta.city;
-                    delete meta.region;
-                    meta.type = "list";
-                    meta.source = "udger";
-                    meta.name = row.code;
-                    metadata = JSON.stringify(meta);
-                    csv = `${ip.toLong(row.ip)}|${ip.toLong(row.ip)}|` + metadata + "\n";
-                    writer.write(csv);
-                }, resolve);
-            })
-        ]);
-        writer.close();
-
+if(isMainThread) {
+    module.exports = (outputFile) => {
         return new Promise((resolve, reject) => {
-            writer.on('finish', () => {
-                console.log('finished extracting datacenter IPs');
-                fs.renameSync(tempFile, outputFile);
-                clearInterval(interval);
-                resolve(pluginName);
+            const worker = new Worker(__filename, {
+                workerData: {outputFile},
+                execArgv: ['--unhandled-rejections=strict']
             });
-            writer.on('error', () => {
-                clearInterval(interval);
-                console.error(pluginName + ' error.');
-                reject(pluginName + " failed");
+            worker.on("message", data=>resolve(data));
+            worker.on("error", error=>{
+                reject(error)
             });
+            worker.on("exit", code=>console.log(`${pluginName} exited with code: ${code}`));
         });
-    } finally {}
+    };
+} else {
+    const axios = require('axios');
+    const fs = require('fs');
+    const sqlite3 = require('sqlite3').verbose();
+    const ip = require('ip-utils');
+    const dbFile = __dirname + '/' + 'udgerdb_v3.dat';
+
+    const main = async (outputFile) => {
+        const interval = setInterval(() => console.log("still working on " + pluginName), 5000);
+        try {
+
+            console.log('extracting datacenter IPs');
+            const tempFile = __dirname + '/' + 'rth595hg34G4gsdfgnu7865y';
+            const writer = fs.createWriteStream(tempFile);
+            let db;
+
+            await Promise.all([
+                new Promise((resolve, reject) => {
+                    db = new sqlite3.Database(dbFile, sqlite3.OPEN_READONLY, (error) => {
+                        if (error) reject(pluginName + ' failed: ' + error.message)
+                        resolve();
+                    });
+                }),
+                new Promise((resolve, reject) => {
+                    db.each("SELECT iplong_from `from`, iplong_to `to`, name from udger_datacenter_range r " +
+                        "inner join udger_datacenter_list l on r.datacenter_id=l.id limit -1", function (err, row) {
+                        const meta = {
+                            type: "list",
+                            source: "udger",
+                            name: "datacenter",
+                            meta: {
+                                "datacenter_name": row.name
+                            }
+                        };
+                        const metadata = JSON.stringify(meta);
+
+                        //let dcName = row.name.replace(/"/g, '\"');
+                        //dcName = dcName.replace(/,/g, '');
+                        const csv = row.from + "|" + row.to + "|" + metadata + "\n"; // -${row.name}
+                        writer.write(csv);
+                    }, resolve);
+                }),
+                new Promise((resolve, reject) => {
+                    db.each("SELECT l.ip, c.ip_classification_code code, l.ip_country_code country, l.ip_city city from udger_ip_list l " +
+                        "inner join udger_ip_class c on l.class_id=c.id limit -1", function (err, row) {
+                        const meta = {
+                            type: "geo",
+                            country: row.country,
+                            city: row.city,
+                            source: "udger"
+                        };
+                        let metadata = JSON.stringify(meta);
+                        let csv = `${ip.toLong(row.ip)}|${ip.toLong(row.ip)}|` + metadata + "\n";
+                        writer.write(csv);
+
+                        delete meta.country;
+                        delete meta.city;
+                        delete meta.region;
+                        meta.type = "list";
+                        meta.source = "udger";
+                        meta.name = row.code;
+                        metadata = JSON.stringify(meta);
+                        csv = `${ip.toLong(row.ip)}|${ip.toLong(row.ip)}|` + metadata + "\n";
+                        writer.write(csv);
+                    }, resolve);
+                })
+            ]);
+            writer.close();
+
+            return new Promise((resolve, reject) => {
+                writer.on('finish', () => {
+                    console.log('finished extracting datacenter IPs');
+                    fs.renameSync(tempFile, outputFile);
+                    clearInterval(interval);
+                    resolve(pluginName);
+                });
+                writer.on('error', () => {
+                    clearInterval(interval);
+                    console.error(pluginName + ' error.');
+                    reject(pluginName + " failed");
+                });
+            });
+        } finally {
+        }
+    }
+
+    main(workerData.outputFile)
+        .then(data => {
+            parentPort.postMessage(`${pluginName} done.`);
+        })
+        .catch(error => {throw error});
 }
